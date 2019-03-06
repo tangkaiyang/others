e084cb813d9071269636eea9bed4236f30716b53
tangkaiyang <945541696@qq.com>
2019-03-06 19:20:43 +0800

    roman to int

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -8,21 +8,7 @@ XL=40, XC=90,
 CD=400, CM=900,
 """
 rms = 'CM'
-def trans(r):
-    if r == "I":
-        return 1
-    elif r == "V":
-        return 5
-    elif r == "X":
-        return 10
-    elif r == "L":
-        return 50
-    elif r == "C":
-        return 100
-    elif r == "D":
-        return 500
-    else:
-        return 1000
+r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
 def romanToInt(s):
     res = 0
@@ -30,11 +16,11 @@ def romanToInt(s):
     i = 0
     while i < len(s):
         i = i + 1
-        if trans(s[-i]) > tmp:
-            tmp = trans(s[-i])
-            res = res + trans(s[-i])
+        if r[s[-i]] > tmp:
+            tmp = r[s[-i]]
+            res = res + r[s[-i]]
         else:
-            res = res - trans(s[-i])
+            res = res - r[s[-i]]
     return res
 
 print(romanToInt(rms))

04038a1e0076af2eed5470dcda03984e5eb21ef5
tangkaiyang <945541696@qq.com>
2019-04-18 19:45:47 +0800

    第一章开发环境配置(未完成)

---
+++ 网络爬虫开发实战/chapter01/test.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time     : 2019/4/18 19:39
+# @Author   : tangky
+# @Site     : 
+# @File     : test.py
+# @Software : PyCharm
+
+from selenium import webdriver
+
+# driver = webdriver.Chrome()
+driver = webdriver.Firefox()

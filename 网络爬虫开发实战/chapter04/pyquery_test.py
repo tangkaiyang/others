816c0f9628506f2c0e88d4365d850dbf9a941f20
tangkaiyang <945541696@qq.com>
2019-04-23 19:50:57 +0800

    第三章 解析库的使用

--- 网络爬虫开发实战/chapter04/pyquery_test.py
+++ 网络爬虫开发实战/chapter04/pyquery_test.py
@@ -49,19 +49,19 @@
 # print(type(lis))
 # print(lis)
 # # 父节点:parent()方法获取某个节点的直接父节点,parents()方法返回所有祖先节点,传入CSS选择器,筛选祖先节点
-html = '''
-<div class="wrap">
-<div id="container">
-<ul class="list">
-<li class="item-0">first item</li>
-<li class="item-1"><a href="link2.html">second item</a></li>
-<li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
-<li class="item-1 active"><a href="link4.html">fourth item</a></li>
-<li class="item-0"><a href="link5.html">fifth item</a></li>
-</ul>
-</div>
-</div>
-'''
+# html = '''
+# <div class="wrap">
+# <div id="container">
+# <ul class="list">
+# <li class="item-0">first item</li>
+# <li class="item-1"><a href="link2.html">second item</a></li>
+# <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+# <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+# <li class="item-0"><a href="link5.html">fifth item</a></li>
+# </ul>
+# </div>
+# </div>
+# '''
 # class="item-0 active"代表有两个class
 # from pyquery import PyQuery as pq
 # doc = pq(html)
@@ -92,8 +92,8 @@ html = '''
 #     print(li, type(li)) # PyQuery类型
 # 6.获取信息:提取节点所包含的信息,属性,文本等
 # # 获取属性:提取到某个PyQuery类型的节点后,就可以调用attr()方法来获取属性
-from pyquery import PyQuery as pq
-doc = pq(html)
+# from pyquery import PyQuery as pq
+# doc = pq(html)
 # a = doc('.item-0.active a')
 # print(a, type(a)) # PyQuery类型
 # print(a.attr('href')) # 调用attr()方法,传入属性的名称
@@ -106,4 +106,91 @@ doc = pq(html)
 # a = doc('a')
 # for item in a.items():
 #     print(item.attr('href'))
-# # 获取文本
+# # 获取文本:调用text()方法,获取内部的文本信息,会忽略掉节点内部包含的所有HTML,只返回纯文字内容
+# a = doc('.item-0.active a')
+# print(a)
+# print(a.text())
+# print(a.html()) # 获取这个节点内部的HTML文本
+# li = doc('li') # 同时选中多个节点
+# print(li.html()) # html()方法返回第一个li节点的内部HTML文本,获取多个需要遍历
+# print(li.text()) # text()方法返回了所有的li节点内部的纯文本,中间用一个空格分隔开,即返回结果是一个字符串
+# print(type(li.text()))
+# 7.节点操作:pyquery提供了一些类方法来对节点进行动态修改,比如为某个节点添加一个class,移除某个节点等,这些操作有时候会为提取信息带来极大的便利
+# # add_class和remove_class动态改变节点的class属性
+# html = '''
+# <div class="wrap">
+# <div id="container">
+# <ul class="list">
+# <li class="item-0">first item</li>
+# <li class="item-1"><a href="link2.html">second item</a></li>
+# <li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+# <li class="item-1 active"><a href="link4.html">fourth item</a></li>
+# <li class="item-0"><a href="link5.html">fifth item</a></li>
+# </ul>
+# </div>
+# </div>
+# '''
+# from pyquery import PyQuery as pq
+# doc = pq(html)
+# li = doc('.item-0.active')
+# print(li)
+# # li.remove_class('active')
+# li.remove_class('active')
+# print(li)
+# li.add_class('active')
+# print(li)
+# # attr,text和html
+# li = doc('.item-0.active')
+# print(li)
+# print(li.attr('class')) # attr(key)获得key对应的属性值
+# li.attr('name', 'link') # attr(key, value)如果key不存在,添加key=value到属性
+# print(li)
+# li.attr('class', 'item-0') # key存在则修改当前属性值
+# print(li)
+# li.text('changed item') # 不传参数,获取节点内纯文本内容,传入参数则修改
+# print(li)
+# li.html('<span>changed item</span>') # 获取节点内HTML文本,如果传入参数,进行赋值
+# print(li)
+# # remove():移除,有时会为信息的提取带来极大的便利
+# html = '''
+# <div class="wrap">
+#     Hello, World
+# <p>This is a paragraph.</p>
+# </div>
+# '''
+# from pyquery import PyQuery as pq
+# doc = pq(html)
+# wrap = doc('.wrap')
+# print(wrap.text())
+# wrap.find('p').remove()
+# print(wrap.text())
+# # 还有很多节点操作的方法,如append(),empty()和prepend(),和jQuery用法完全一致
+# 8.伪类选择器,例如选择第一个节点,最后一个节点,奇偶数节点,包含某一文本的节点等
+html = '''
+<div class="wrap">
+<div id="container">
+<ul class="list">
+<li class="item-0">first item</li>
+<li class="item-1"><a href="link2.html">second item</a></li>
+<li class="item-0 active"><a href="link3.html"><span class="bold">third item</span></a></li>
+<li class="item-1 active"><a href="link4.html">fourth item</a></li>
+<li class="item-0"><a href="link5.html">fifth item</a></li>
+</ul>
+</div>
+</div>
+'''
+from pyquery import PyQuery as pq
+
+doc = pq(html)
+li = doc('li:first-child')  # 第一个li节点
+print(li)
+li = doc('li:last-child')  # 最后一个li节点
+print(li)
+li = doc('li:nth-child(2)')  # 第二个li节点
+print(li)
+li = doc('li:gt(2)')  # 第三个li之后的li节点
+print(li)
+li = doc('li:nth-child(2n)')  # 偶数位置的li节点
+print(li)
+li = doc('li:contains(second)')  # 包括second文本的li节点
+print(li)
